65a89b7ff41dda7e91480e53adbbc698ff33ad57
David Goldfarb <deg@degel.com>
2015-04-22 15:54:57 +0300

    Add toy search page.

--- src/cljs/oldnews/core.cljs
+++ src/cljs/oldnews/core.cljs
@@ -10,11 +10,58 @@
 ;; -------------------------
 ;; Views
 
+(defonce the-state (atom nil))
+
+(defn set-state! [key value]
+  (swap! the-state assoc key value))
+
+(defn get-state [key]
+  (key @the-state))
+
+(defn input-element
+  "An input element which updates its value on change"
+  [id name type]
+  [:input {:id id
+           :name name
+           :class "form-control"
+           :type type
+           :required ""
+           :value (get-state id)
+           :on-change #(set-state! id (-> % .-target .-value))}])
+
+
+(defn search-input [state-id]
+  (input-element state-id "searchVal" "text"))
+
+(defn launch-search []
+  (let [search-string (get-state :text1)]
+    (set-state! :current-search-string search-string))
+  nil
+  )
+
 (defn home-page []
-  [:div [:h2 "Old News browser"]
-   [:div [:p "Tomorrow's way to see yesterday today"]]
-   [:div [:a {:href "#/about"} "go to about page"]]
-   [:div [:a {:href "#/debug"} "See internal app state"]]])
+  (fn []
+    [:div [:h2 "Old News browser"]
+     [:div [:p "Tomorrow's way to see yesterday today"]]
+     ;; [TODO] Understand why form causes page refresh. Seems to be related
+     ;; to it adding search string as a param to the page URL
+     ;;[:form
+      [:fieldset
+       [:legend "Search for some good news"]
+       [:label {:for :text1} "Enter text: "]
+       [search-input :text1]
+       [:br]
+       [:button {:type "submit"
+                 :class "btn btn-default"
+                 :id :search
+                 :on-click launch-search}
+        "Search now"]]
+     ;;]
+     [:div {:id "status"}
+      [:em "Searching for " (get-state :current-search-string)]
+      ]
+     [:div [:a {:href "#/about"} "go to about page"]]
+     [:div [:a {:href "#/debug"} "See internal app state"]]]))
 
 (defn about-page []
   [:div [:h2 "About Old News"]
